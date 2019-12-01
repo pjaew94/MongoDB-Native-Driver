10d0c3c53e25cd77d193ae1a82d90e0eaf6830fb
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2019-12-01 14:22:46 -0500

    mongoose practice

--- app.js
+++ app.js
@@ -1,65 +1,71 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  findDocuments(db, function() {
-    client.close();
-  });
-});
-
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-    {
-      name : "Apple",
-      score: 8,
-      review: "Great fruit"
-    },
-    {
-      name : "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-    {
-      name : "Banana",
-      score: 9,
-      review: "Great stuff!"
-    }
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useUnifiedTopology: true, useNewUrlParser: true },  )
 
 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits)
-    callback(fruits);
-  });
-}
+const fruitSchema = new mongoose.Schema ({
+  name : {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  review: String
+})
+
+const Fruit = mongoose.model("Fruit", fruitSchema)
+
+const pineapple = new Fruit ({
+  name: "Pineapple",
+  rating: 8,
+  review: "Its good"
+})
+
+ 
+
+pineapple.save()
+
+
+
+const peopleSchema = new mongoose.Schema ({
+  name: String,
+  age: Number,
+  favoriteFruit: fruitSchema
+})
+
+const People = mongoose.model("People", peopleSchema)
+
+const people = new People ({
+  name: "John",
+  age: 37,
+})
+
+// people.save();
+
+
+
+// Fruit.find(function(err, fruits) {
+
+//   if(err) {
+//     console.log(err)
+//   } else {
+
+//     mongoose.connection.close()
+  
+//     fruits.forEach(function(fruit) {
+//       console.log(fruit.name)
+//     })
+//   }
+// })
+
+
+People.updateOne({_id: "5de409e83d4b26246873faf0"}, {favoriteFruit: pineapple}, function(err) {
+  if(err){
+    console.log(err)
+  }else{
+    console.log("Successful")
+  }
+})
